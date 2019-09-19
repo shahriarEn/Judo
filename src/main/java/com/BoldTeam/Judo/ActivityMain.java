14a911924409b7c80f592e7a04a49e8eb6ca159a
shahriar <shahriarenayaty1376@gmail.com>
2019-09-19 23:18:44 +0430

    add splash activity

---
+++ src/main/java/com/BoldTeam/Judo/ActivityMain.java
@@ -0,0 +1,24 @@
+package com.BoldTeam.Judo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ActivityMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView shahriar = findViewById(R.id.shahriar);
+        shahriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ActivityMain.this,UnityPlayerActivity.class));
+            }
+        });
+    }
+}
